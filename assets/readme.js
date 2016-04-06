fc8b85f370adf449a5b68fbbf341018007dcbd29
Marcin Ignac <marcin.ignac@gmail.com>
2016-04-06 19:17:52 +0100

    Init

---
+++ assets/readme.js
@@ -0,0 +1,30 @@
+function loadTextBrowser(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onreadystatechange = function (e) {
+    if (request.readyState == 4) {
+      if (request.status == 200) {
+        if (callback) {
+          callback(null, request.responseText);
+        }
+      }
+      else {
+        callback('WebIO.loadTextFile error : ' + request.statusText, null);
+      }
+    }
+  };
+  request.send(null);
+}
+
+function showReadme(repo, name) {
+    var readmeViewer = document.getElementById('readmeViewer');
+
+    loadTextBrowser('readme/' + name + '/README.md', function(err, md) {
+        var html = marked(md);;
+        var a = '<a href="' + repo + '" target="blank">' + repo + '</a>';
+        readmeViewer.innerHTML = a + html;
+        //console.log(md);
+        //console.log(marked(md.replace(/\n/g, '<br/>')));
+    })
+
+}
