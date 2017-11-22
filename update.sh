fcc930f73f0cf7da776898a8c81d552059d1e498
Marcin Ignac <marcin.ignac@gmail.com>
2017-11-22 13:37:05 +0000

    Update repos list

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 sed '/^\#/d' repos.txt > repos.txt.tmp
 cat repos.txt.tmp | DEBUG=ecosystem-docs ./node_modules/.bin/ecosystem-docs sync
-cat repos.txt.tmp | ./node_modules/.bin/ecosystem-docs read > repos.json.txt
+cat repos.txt.tmp | DEBUG=ecosystem-docs ./node_modules/.bin/ecosystem-docs read > repos.json.txt
 rm repos.txt.tmp
 node index.js
