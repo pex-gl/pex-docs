22f6a8c13968321bd2cd7b3baba85b922845b604
Marcin Ignac <marcin.ignac@gmail.com>
2017-11-22 13:21:01 +0000

    Add logging to ecosystem-docs refresh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 sed '/^\#/d' repos.txt > repos.txt.tmp
-cat repos.txt.tmp | ./node_modules/.bin/ecosystem-docs sync
+cat repos.txt.tmp | DEBUG=ecosystem-docs ./node_modules/.bin/ecosystem-docs sync
 cat repos.txt.tmp | ./node_modules/.bin/ecosystem-docs read > repos.json.txt
 rm repos.txt.tmp
